1a07b9fce1f350ff945a22aabdac19593df773a1
Dave Woodall <dave.woodall@livingsocial.com>
2014-10-27 07:37:17 -0600

    Create does new and save.

--- railsApp/spec/models/goal_spec.rb
+++ railsApp/spec/models/goal_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 describe Goal do
 
   it 'is valid with a name and circle' do
-    goal = Goal.new name: 'learn to code', circle: 'personal'
+    goal = Goal.create name: 'learn to code', circle: 'personal'
     expect(goal).to be_valid
   end
 
   it 'is invalid without a name' do
-    goal = Goal.new name: nil
+    goal = Goal.create name: nil
     goal.valid?
     expect(goal).not_to be_valid
   end
   it 'takes an optional category' do
-    goal = Goal.new(
+    goal = Goal.create(
       name: 'date night',
       circle: 'relational',
       category: 'activity'
@@ -22,8 +22,16 @@ describe Goal do
   end
 
   it 'requires a circle to be selected' do
-    goal = Goal.new name: 'swim', circle: nil
+    goal = Goal.create name: 'swim', circle: nil
     goal.valid?
     expect(goal).not_to be_valid
+  end
+
+  it 'retrieves matching goals' do
+    date = Goal.create name: 'date night', circle: 'relational'
+    play = Goal.create name: 'play night', circle: 'relational'
+    ignore_this  = Goal.create name: 'play day',   circle: 'responsibility'
+
+    expect(Goal.find('night')).to eq [ date, play ]
   end
 end
