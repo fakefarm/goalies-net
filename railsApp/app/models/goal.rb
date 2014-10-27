e900710ff4a381a2e0916afa6769ed71b1c5fb2c
Dave Woodall <dave.woodall@livingsocial.com>
2014-10-27 07:37:28 -0600

    Simple #find

--- railsApp/app/models/goal.rb
+++ railsApp/app/models/goal.rb
@@ -1,4 +1,8 @@
 class Goal < ActiveRecord::Base
   validates :name, presence: true
   validates :circle, presence: true
+
+  def self.find(goal)
+    where("name LIKE ?", "%#{goal}%").order(:name)
+  end
 end
