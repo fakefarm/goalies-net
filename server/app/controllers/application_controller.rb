2ad38eb07b8f98681e8244afc269403fb9e735b7
Dave Woodall <dave.woodall@livingsocial.com>
2014-10-30 07:39:44 -0600

    Set Headers for Client & Server to play nicely

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :null_session
+
+  before_filter :set_cors_headers
+  before_filter :cors_preflight
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin']  = AppConfig['defaults']['client']['origin']
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = '3628800'
+  end
+
+  def cors_preflight
+    head(:ok) if request.method == :options
+  end
 end
--- server/config/application.rb
+++ server/config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+AppConfig = YAML.load(File.read(File.expand_path('../app_config.yml', __FILE__)))
+
 module RailsApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
